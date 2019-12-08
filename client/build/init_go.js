f0df47d3509786987ebe9aa06939958792e3136d
Ayomide Onigbinde <onigbindeayomide@gmail.com>
2019-12-08 22:17:06 +0100

    all: deploy to heroku

---
+++ client/build/init_go.js
@@ -0,0 +1,3 @@
+(() => {
+    window.go = new Go()
+})()
---
+++ client/build/precache-manifest.2b4f137e919f3de8b3d2aed6174d2b68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087fd3e960b9519795aaf8534a971931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "798e589d0b239b663629a2aa9d1ce902",
+    "url": "/init_go.js"
+  },
+  {
+    "revision": "1d5acf17e2e1ed58be2f",
+    "url": "/static/css/2.44d09fed.chunk.css"
+  },
+  {
+    "revision": "204270b04705c9141dca",
+    "url": "/static/css/main.61e22c58.chunk.css"
+  },
+  {
+    "revision": "1d5acf17e2e1ed58be2f",
+    "url": "/static/js/2.607aa33a.chunk.js"
+  },
+  {
+    "revision": "204270b04705c9141dca",
+    "url": "/static/js/main.7d04c936.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  },
+  {
+    "revision": "2e82b68890b98d579a77943d96f53435",
+    "url": "/wasm_exec.js"
+  }
+]);
