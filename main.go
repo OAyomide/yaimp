4930fc4847349260dbde263c6cafd42ce048242c
Ayomide Onigbinde <onigbindeayomide@gmail.com>
2019-12-08 22:52:44 +0100

    main: heroku port debug isshhh

--- main.go
+++ main.go
@@ -7,15 +7,12 @@ import (
 )
 
 func main() {
-	var port string
 	// get port env var
-	osport := os.Getenv("PORT")
-	if osport == "" {
-		port = "11000"
-	} else {
-		port = osport
-	}
+	port := os.Getenv("PORT")
 
+	if port == "" {
+		log.Fatal("PORT NOT SPECIFIED")
+	}
 	http.Handle("/", http.FileServer(http.Dir("./client/build")))
 	log.Printf("Server up and running on PORT %s", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
